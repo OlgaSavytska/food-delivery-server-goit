bcbbe1453cd520e6ea94673d0e6e733cd3908785
OlgaSavytska <o.savytska77@gmail.com>
2019-11-14 17:33:56 +0200

    master clear

--- src/routes/products/products.js
+++
@@ -1,9 +0,0 @@
-const productsDB = require('../../db/products/all-products.json');
-
-const products = (response) => {
-    response.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    response.write(JSON.stringify(productsDB));
-}
-module.exports = products;
